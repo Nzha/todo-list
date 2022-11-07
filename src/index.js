fd7cb7c3d6b1c556b2fb7e0a7db68b434dd23b5e
Adrien <adrien.biz@gmail.com>
2022-11-07 20:17:13 +0400

    Feat: toggle menu on/off

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+function menuDisplay() {
+    const hideMenuBtn = document.querySelector('#hide-menu-btn');
+    const sideBarDiv = document.querySelector('.sidebar');
+
+    hideMenuBtn.addEventListener('click', hideMenu);
+
+    function hideMenu() {
+        sideBarDiv.classList.toggle('hidden');
+    }
+
+}
+
+menuDisplay();
