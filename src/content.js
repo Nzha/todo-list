b0095437c74c80184813e4a15b8609a896802d0d
Adrien <adrien.biz@gmail.com>
2022-12-02 13:21:01 +0400

    Feat: load tasks based on their due date

--- src/content.js
+++ src/content.js
@@ -1,5 +1,6 @@
 import createEl from './functions';
 import createAddTaskBtn, { createTaskEl } from './taskManagement';
+import { parseISO, isToday, isThisWeek } from 'date-fns';
 
 function loadContent(title) {
     const content = document.querySelector('.content');
@@ -15,21 +16,35 @@ function loadContent(title) {
 
     // Check for locally stored tasks
     const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    const todaysTasks = storedTasks.filter(task => isToday(parseISO(task.dueDate)));
+    const thisWeekTasks = storedTasks.filter(task => isThisWeek(parseISO(task.dueDate)));
 
     if (storedTasks) {
-        storedTasks.forEach(storedTask => {
-            // Create corresponding elements
-            createTaskEl(storedTask, true)
-
-            // Display checked checkboxes as checked
-            if (storedTask.status == 'checked') {
-                const checkbox = document.querySelector(`#task${storedTask.id} .task-checkbox`);
-                checkbox.checked = true;
-            }
-        })
+        if (title == 'All' && storedTasks) {
+            loadTasks(storedTasks);
+        } else if (title == 'Today' && todaysTasks) {
+            loadTasks(todaysTasks);
+        } else if (title == 'Week' && thisWeekTasks) {
+            loadTasks(thisWeekTasks);
+        } else {
+            console.log('Empty');
+        }
     }
 
     createAddTaskBtn();
+}
+
+function loadTasks(tasks) {
+    tasks.forEach(task => {
+        // Create corresponding elements
+        createTaskEl(task, true)
+
+        // Display checked checkboxes as checked
+        if (task.status == 'checked') {
+            const checkbox = document.querySelector(`#task${task.id} .task-checkbox`);
+            checkbox.checked = true;
+        }
+    });
 }
 
 export default loadContent;
